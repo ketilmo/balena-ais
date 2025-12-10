d4e405a35acc3321e39ac323cd60d191a23d557b
Ketil Moland Olsen <ketil@ketilmo.com>
2025-12-10 11:53:08 +0100

    Added idle support.

--- ais/start.sh
+++ ais/start.sh
@@ -3,8 +3,10 @@ set -e
 
 # Check if service has been disabled through the DISABLED_SERVICES environment variable.
 if [[ ",$(echo -e "${DISABLED_SERVICES}" | tr -d '[:space:]')," = *",$BALENA_SERVICE_NAME,"* ]]; then
-        echo "$BALENA_SERVICE_NAME is manually disabled."
-        sleep infinity
+        echo "$BALENA_SERVICE_NAME is manually disabled. Sending request to stop the service:"
+        curl --fail --retry 86400 --retry-delay 1 --retry-all-errors --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v2/applications/$BALENA_APP_ID/stop-service?apikey=$BALENA_SUPERVISOR_API_KEY" -d '{"serviceName": "'$BALENA_SERVICE_NAME'"}'
+        echo " "
+        balena-idle
 fi
 
 # Verify that all the required varibles are set before starting up the application.
@@ -77,7 +79,7 @@ if [ "$feed_error" = true ]; then
     echo " "
     echo "ERROR: One or more AIS feed configurations are invalid, aborting..."
     echo " "
-    sleep infinity
+    balena-idle
 fi
 
 if [ -z "$AIS_FEEDS" ]; then
@@ -92,7 +94,7 @@ if [ "$missing_variables" = true ]
 then
         echo "Settings missing, aborting..."
         echo " "
-        sleep infinity
+        balena-idle
 fi
 
 echo "Settings verified, proceeding with startup."
